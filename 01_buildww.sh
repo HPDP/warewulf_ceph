d345507aafc833589fcaa4002d15156def67d97c
ruilinchu <ruilinchu@gmail.com>
2015-09-12 11:19:10 -0700

    Update 01_buildww.sh

--- 01_buildww.sh
+++ 01_buildww.sh
@@ -1,9 +1,40 @@
 #!/bin/bash
 
 #using centos 7
+## disable selinux
+sed -i 's+=enforcing+=disabled+g' /etc/selinux/config
+
+## disable firewall
+#firewall-cmd --zone=public --add-port=6789/tcp --permanent
+#firewall-cmd --zone=public --add-port=6800-7100/tcp --permanent
+#firewall-cmd --reload
+systemctl disable firewalld
+
+## ntp
+yum install -y ntp ntpdate ntp-doc
+systemctl enable ntpd
+
+useradd ceph
+echo bwv988 | passwd ceph --stdin
+echo "ceph ALL = (root) NOPASSWD:ALL" | tee /etc/sudoers.d/ceph
+chmod 0440 /etc/sudoers.d/ceph
+## visudo to edit requiretty for allow sudo without passwd
+## edit /etc/sudoers to change this line to: Defaults:ceph !requiretty
+sed -i s'/Defaults    requiretty/Defaults:ceph    !requiretty'/g /etc/sudoers
+
+## ceph repo, update all nodes
+rpm -Uhv http://ceph.com/rpm-hammer/el7/noarch/ceph-release-1-0.el7.noarch.rpm
+
+yum -y update 
+yum -y install epel-release
+yum -y install snappy leveldb gdisk python-argparse gperftools-libs
+yum -y install ceph --disablerepo=epel
+
+chkconfig ceph on
+
 yum -y group install 'Development tools' 
 yum -y install wireshark tcpdump perl-DBD-MySQL mariadb nfs-utils ntp perl-Term-ReadLine-Gnu tftp tftp-server pigz 
-yum -y install epel-release libselinux-devel libacl-devel libattr-devel
+yum -y install libselinux-devel libacl-devel libattr-devel
 
 # Build Warewulf from SVN
 BUILD_DIR=/root/rpmbuild
@@ -41,8 +72,8 @@ yum -y install json-c-devel json-devel sqlite-devel
 build_it $WW_DIR/monitor $BUILD_DIR
 yum -y install $RPM_DIR/x86_64/warewulf-monitor*.rpm
 
-build_it $WW_DIR/ipmi $BUILD_DIR
-yum -y install $RPM_DIR/x86_64/warewulf-ipmi*.rpm
+#build_it $WW_DIR/ipmi $BUILD_DIR
+#yum -y install $RPM_DIR/x86_64/warewulf-ipmi*.rpm
 
 
 
