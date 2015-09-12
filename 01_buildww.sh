5b08a3d812464d8bb258a06ef5f36acf635d0019
Jimi Chu <jimichu@jimis-mbp.attlocal.net>
2015-09-11 17:04:01 -0700

    update

--- 01_buildww.sh
+++ 01_buildww.sh
@@ -2,7 +2,7 @@
 
 yum -y group install 'Development tools' 
 yum -y install wireshark tcpdump perl-DBD-MySQL mariadb nfs-utils ntp perl-Term-ReadLine-Gnu tftp tftp-server pigz 
-yum -y install libselinux-devel libacl-devel libattr-devel
+yum -y install epel-release libselinux-devel libacl-devel libattr-devel
 
 # Build Warewulf from SVN
 BUILD_DIR=/root/rpmbuild
@@ -19,19 +19,30 @@ mkdir /root/warewulf
 svn co https://warewulf.lbl.gov/svn/trunk/ /root/warewulf
 
 build_it $WW_DIR/common $BUILD_DIR
-yum install -y $RPM_DIR/noarch/warewulf-common-*.el7.centos.noarch.rpm
+yum install -y $RPM_DIR/noarch/warewulf-common-*.rpm
 
 build_it $WW_DIR/provision $BUILD_DIR
-yum install -y $RPM_DIR/x86_64/warewulf-provision-*.el7.centos.noarch.rpm
+yum install -y $RPM_DIR/x86_64/warewulf-provision-*.rpm
 
 build_it $WW_DIR/cluster $BUILD_DIR
-yum install -y $RPM_DIR/x86_64/warewulf-cluster-*.el7.centos.noarch.rpm
+yum install -y $RPM_DIR/x86_64/warewulf-cluster-*.rpm
 
+## remove debian.tmpl from Makefil* in vnfs/libexec/wwmkchroot
+sed -i 's+debian.tmpl+ +g' $WW_DIR/vnfs/libexec/wwmkchroot/Makefil*
 build_it $WW_DIR/vnfs $BUILD_DIR
-yum install -y $RPM_DIR/x86_64/warewulf-vnfs-*.el7.centos.noarch.rpm
+yum install -y $RPM_DIR/noarch/warewulf-vnfs-*.rpm
+
+# monitor
+ln -s /usr/lib64/libjson.so.0 /usr/lib64/libjson.so 
+ln -s /usr/lib64/libjson-c.so.2 /usr/lib64/libjson-c.so
+ln -s /usr/lib64/libsqlite3.so.0 /usr/lib64/libsqlite3.so
+yum -y install json-c-devel json-devel sqlite-devel
+build_it $WW_DIR/monitor $BUILD_DIR
+yum -y install $RPM_DIR/x86_64/warewulf-monitor*.rpm
 
 # Build not working yet on el7
-#build_it $WW_DIR/ipmi $BUILD_DIR
+build_it $WW_DIR/ipmi $BUILD_DIR
+yum -y install $RPM_DIR/x86_64/warewulf-ipmi*.rpm
 
 
 
