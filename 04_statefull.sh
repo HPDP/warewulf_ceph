d3b5a66285f739fdcd3c486061b940834ad08ce3
ruilinchu <ruilinchu@gmail.com>
2015-09-15 10:23:34 -0700

    update

--- 04_statefull.sh
+++ 04_statefull.sh
@@ -2,4 +2,10 @@
 
 wwmkchroot centos-7 /var/chroots/centos-7
 yum -y --installroot=/var/chroots/centos-7 install kernel grub
+
+## so the nodes have root passwd set
+cp -f /etc/passwd /var/chroots/centos-7/etc
+cp -f /etc/group /var/chroots/centos-7/etc
+cp -f /etc/shadow /var/chroots/centos-7/etc
+
 wwvnfs --chroot /var/chroots/centos-7
