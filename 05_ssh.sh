de5e7f5a8a5fd9c6b6b2cbf5c64e2ffb2a3dc222
ruilinchu <ruilinchu@gmail.com>
2015-09-16 12:55:18 -0700

    update

--- 05_ssh.sh
+++ 05_ssh.sh
@@ -9,14 +9,12 @@ cp /etc/ssh/ssh_known_hosts /var/chroots/centos-7/etc/ssh
 
 ## install nptd to comupte nodes to sync time with master
 yum --tolerant --installroot /var/chroots/centos-7 -y install ntp
-yum --tolerant --installroot /var/chroots/centos-7 -y install ~/rpmbuild/RPMS/x86_64/warewulf-monitor*.rpm
+yum --tolerant --installroot /var/chroots/centos-7 -y install ~/rpmbuild/RPMS/x86_64/warewulf-monitor-node*.rpm
 
 sed -i '/.pool.ntp.org/c\server 172.16.2.250' /var/chroots/centos-7/etc/ntp.conf
 
 chroot /var/chroots/centos-7/ systemctl enable ntpd
+chroot /var/chroots/centos-7/ chkconfig collector on
+sed -i 's+127.0.0.1+172.16.2.250+g' /var/chroots/centos-7/etc/warewulf/monitor.conf
 
-sed -i '/WAREWULF_MASTER/c\WAREWULF_MASTER=172.16.2.250' /var/chroots/centos-7/etc/sysconfig/wulfd.conf
-
-HOSTLIST=`grep eth0 /etc/hosts | awk '{print $3}' | paste -d, -s`
-sed -i "/WAREWULFD_HOSTS/c\WAREWULFD_HOSTS=$HOSTLIST " /etc/sysconfig/wwproxy.conf
 
