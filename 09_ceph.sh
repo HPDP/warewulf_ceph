003dfe557ed463e85c1d1a7d204aeb682c7fe4d5
ruilinchu <ruilinchu@gmail.com>
2015-09-17 15:21:38 -0700

    update

--- 09_ceph.sh
+++ 09_ceph.sh
@@ -10,12 +10,12 @@ pdsh cp -fr /home/ceph/ceph /etc
 sleep 5
 
 ssh c002 "/home/ceph/add_mon.sh c002 172.16.0.2"
-sleep 3
+sleep 5
 ssh c003 "/home/ceph/add_mon.sh c003 172.16.0.3"
 
 ssh c001 "/home/ceph/add_osd.sh"
-sleep 3
+sleep 5
 ssh c002 "/home/ceph/add_osd.sh"
-sleep 3
+sleep 5
 ssh c003 "/home/ceph/add_osd.sh"
 
