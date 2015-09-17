318ef152e16c2d0eb21863fa7f86f6e3819f62d1
ruilinchu <ruilinchu@gmail.com>
2015-09-17 16:45:00 -0700

    update

--- clear_ceph.sh
+++ clear_ceph.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+pdsh service ceph stop
 pdsh rm -fr /etc/ceph
 pdsh rm -fr /var/lib/ceph
 pdsh rm -fr /tmp/c00*
 pdsh rm -fr /tmp/cep*
 pdsh rm -fr /tmp/mon*
-pdsh service ceph stop
+pdsh reboot
