a4a8bc693f8f1e90571fb4e1d9b5b77d7477544b
ruilinchu <ruilinchu@gmail.com>
2015-09-17 14:43:00 -0700

    update

--- add_mon.sh
+++ add_mon.sh
@@ -5,7 +5,7 @@ nodeip=$2
 
 mkdir -p /var/lib/ceph/mon/ceph-$node /tmp/$node
 ceph auth get mon. -o /tmp/$node/monkeyring
-ceph mon sgetmap -o /tmp/$node/monmap
+ceph mon getmap -o /tmp/$node/monmap
 ceph-mon -i $node --mkfs --monmap /tmp/$node/monmap --keyring /tmp/$node/monkeyring
 
 ceph mon add $node $nodeip:6789
--- clear_ceph.sh
+++ clear_ceph.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 pdsh rm -fr /etc/ceph
-pdsh rm -fr /var/lib/cep*
+pdsh rm -fr /var/lib/ceph
 pdsh rm -fr /tmp/c00*
 pdsh rm -fr /tmp/cep*
 pdsh rm -fr /tmp/mon*
