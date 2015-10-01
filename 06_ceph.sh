88defb7e6a72f61bfc4ad52bee572ac4b1f3c9e0
ruilinchu <ruilinchu@gmail.com>
2015-10-01 12:35:40 -0400

    update

--- 06_ceph.sh
+++ 06_ceph.sh
@@ -2,7 +2,7 @@
 
 ## install ceph to vnfs
 
-yum --tolerant --installroot /var/chroots/centos-7 -y install snappy leveldb gdisk python-argparse gperftools-libs ntp ntpdate ntp-doc 
+yum --tolerant --installroot /var/chroots/centos-7 -y install snappy leveldb gdisk python-argparse gperftools-libs ntp ntpdate ntp-doc attr
 yum --tolerant --installroot /var/chroots/centos-7 -y install ceph --disablerepo=epel
 
 chroot /var/chroots/centos-7/ systemctl disable firewalld
