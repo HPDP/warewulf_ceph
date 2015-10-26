78f284bef091449211759ce3df25b858ec3e64b2
ruilinchu <ruilinchu@gmail.com>
2015-10-26 12:19:34 -0700

    update

--- 09_ceph.sh
+++ 09_ceph.sh
@@ -3,34 +3,34 @@
 cp first_mon.sh /home/ceph
 cp add_mon.sh /home/ceph
 cp add_osd.sh /home/ceph
-sleep 3
+sleep 15
 
 ssh c001 "/home/ceph/first_mon.sh"
-sleep 5
+sleep 15
 pdsh cp -fr /home/ceph/ceph /etc
 cp -fr /home/ceph/ceph /etc
 
 ssh c002 "/home/ceph/add_mon.sh c002 172.16.0.2"
-sleep 5
+sleep 15
 ssh c003 "/home/ceph/add_mon.sh c003 172.16.0.3"
 
 ssh c001 "/home/ceph/add_osd.sh"
-sleep 5
+sleep 15
 ssh c002 "/home/ceph/add_osd.sh"
-sleep 5
+sleep 15
 ssh c003 "/home/ceph/add_osd.sh"
 
 echo waiting --------
 echo
-sleep 10
+sleep 200
 ceph osd pool set rbd pg_num 256
 echo waiting --------
 echo
-sleep 20
+sleep 200
 ceph osd pool set rbd pgp_num 256
 echo waiting --------
 echo
-sleep 20
+sleep 200
 
 ceph -s
 
